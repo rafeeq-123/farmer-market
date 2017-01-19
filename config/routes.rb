fe4c59b4b0c8b0bd9453d26f4241cbac86210332
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2017-01-19 15:34:33 -0800

    added get dollars route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   get "welcome/index"
   get "farmers/weather"
+  get "farmers/dollars"
   root "welcome#index"
   resources :farmers
 end
