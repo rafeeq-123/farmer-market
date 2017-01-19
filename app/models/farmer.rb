96c8a40baf0de315f748d8413c5313fa000b2401
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2017-01-19 12:09:34 -0800

    added a less verabose way has_attached and for the validates content type

--- app/models/farmer.rb
+++ app/models/farmer.rb
@@ -1,9 +1,11 @@
 class Farmer < ApplicationRecord
   attr_accessor :avatar_file_name
   belongs_to :user
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
-  validates_attachment :avatar, 
-    content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
+  has_attached_file :avatar, styles: {
+    thumb: '100x100>',
+    square: '600x600#',
+    medium: '300x300>'
+  }
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   
-
 end
