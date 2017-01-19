30ddeb017fe08fd36b269168479a29ee2a54c572
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2017-01-19 15:08:06 -0800

    the reason why my uploaded photos were not working was because of the attr_accessor and with removing that the user can now upload photos

--- app/models/farmer.rb
+++ app/models/farmer.rb
@@ -1,11 +1,10 @@
 class Farmer < ApplicationRecord
-  attr_accessor :avatar_file_name
   belongs_to :user
   has_attached_file :avatar, styles: {
     thumb: '100x100>',
     square: '600x600#',
     medium: '300x300>'
-  }
+  }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   
 end
