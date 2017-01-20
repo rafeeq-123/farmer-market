b03f6cdb4c6f3d35fd89ee9b3c99267e0c158d35
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2017-01-19 16:21:32 -0800

    added a method that gives 29517 instead of the dollar amount depending on the average price of things

--- app/helpers/farmers_helper.rb
+++ app/helpers/farmers_helper.rb
@@ -1,2 +1,13 @@
 module FarmersHelper
+  def dollar_sign
+    if @farmer.price_range == 5..15
+        "$"
+      elsif @farmer.price_range.to_i == 15..25
+        "$$"
+      elsif @farmer.price_range.to_i == 25..35
+        "$$$"
+      else @farmer.price_range.to_i == 35..1000
+        "$$$$"
+      end
+   end          
 end
