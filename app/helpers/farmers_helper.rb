14103cacf3040540ccad392563bcb6e57d22d456
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2017-01-27 10:35:38 -0800

    chnaged the helper method because the string was not being changed into a format that allowed me to convert a range

--- app/helpers/farmers_helper.rb
+++ app/helpers/farmers_helper.rb
@@ -1,12 +1,15 @@
 module FarmersHelper
   def dollar_sign
-    if @farmer.price_range.to_i == 5..15
+    binding.pry
+    ranged_num = @farmer.price_range.split("-").map(&:to_i)
+    range = ranged_num[0]..ranged_num[1]
+    if range.include?(1..20)
       "$" 
-    elsif @farmer.price_range.to_i == 15..25
+    elsif range.include?(20..35)
       "$$" 
-    elsif @farmer.price_range.to_i == 25..35
+    elsif range.include?(35..50)
       "$$$" 
-    else @farmer.price_range.to_i == 35..1000
+    elsif range.include?(50..ranged_num[1])
       "$$$$" 
     end
   end          
