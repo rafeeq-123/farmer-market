c88975ca5539b30afa233e3e060cfbb7f1b5be68
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2017-01-27 14:24:47 -0800

    adding paperclip generated files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170119014127) do
+ActiveRecord::Schema.define(version: 20170119180530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,10 +20,13 @@ ActiveRecord::Schema.define(version: 20170119014127) do
     t.string   "schedule"
     t.text     "speciality"
     t.string   "price_range"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
     t.integer  "user_id"
-    t.string   "avatar"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
